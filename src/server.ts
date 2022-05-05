4f3a6a46c050b4be4eee42f6bbe869684b3103cc
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-05-05 20:32:13 -0300

    feat: create feddback, send email

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,44 @@
 import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-  console.log(req.body);
-  return res.send("Hello World");
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8e1c0686dae033",
+    pass: "f6abc106aaa070"
+  }
+});
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedack.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Pedro Lopes <pedrolopesgrc@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join("\n")
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
